1ecf2e48d1e72a71ffc63df120613c915c09d42a
Jacob Birk <jacob.birk@markmonitor.com>
2017-06-23 14:03:26 -0700

    initial

---
+++ apirequest.js
@@ -0,0 +1,19 @@
+const https = require("https");
+
+const token = process.env["API_TOKEN"];
+const url = process.env["API_URL"];
+
+function callAPI(url, token) {
+    console.log(`Request api url ${url}`);
+    https.get(`${url}?token=${token}`, (res) => {
+        res.on("data", (data) => {
+            console.log(data);
+        });
+    }).on("error", err => {
+        console.error("Error");
+        console.error(data);
+    });
+}
+callAPI(url, token);
+
+// API_TOKEN=abcdeftoken API_URL=https://blabla node ./apirequest.js
